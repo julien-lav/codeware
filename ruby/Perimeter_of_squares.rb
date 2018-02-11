a8e6cdaf12e78f14363e1cc64305435970b6b1c7
julien-lav <julien.laville@hotmail.fr>
2018-02-11 01:01:36 +0100

    fibonacci

--- ruby/Perimeter_of_squares.rb
+++ ruby/Perimeter_of_squares.rb
@@ -1,8 +1,19 @@
 def perimeter(n)
-  arr = [0,1,1]
-  for i in (3..n+1) do 
-     arr << (arr[i-1] + arr[i-2])
-     i += 1
+  if n == 0 
+  	return p 0
+  elsif n == 1 
+  	return p 1
+  else
+    arr = [0,1,1]
+    for i in (3..n+1) do 
+       arr << (arr[i-1] + arr[i-2])
+       i += 1
+    end
+    return p 4 * (arr.reduce(:+))
   end
-  return 4 * (arr.reduce(:+))
 end
+
+perimeter(0)
+perimeter(1)
+perimeter(5)
+perimeter(7)
